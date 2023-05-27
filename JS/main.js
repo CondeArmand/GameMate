e35282a96308d6cc78b17a9bea2d29a0ad0d83da
Gabriell1507 <gabrielmarcosbguimaraes@gmail.com>
2023-05-27 16:20:57 -0300

    correções

--- JS/main.js
+++ JS/main.js
@@ -47,3 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
    }
 });
 
+const btnExpandir = document.querySelector('.btn-expandir');const menuLateral = document.querySelector('.menu-lateral');const menu = document.querySelector('.menu');btnExpandir.addEventListener('click', () => {menuLateral.classList.toggle('active');menu.classList.toggle('active');});
+document.addEventListener('click', (event) => {const isClickInside = menuLateral.contains(event.target) || btnExpandir.contains(event.target);
+if (!isClickInside) {menuLateral.classList.remove('active');menu.classList.remove('active');}});
