d8798622a2293c0a5990e47cc3ca2606be67eb28
Conde Armand <neoarmandosousa1010@hotmail.com>
2023-05-29 09:51:13 -0300

    Bug no google auth

--- JS/service-worker.js
+++ JS/service-worker.js
@@ -2,20 +2,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('v1').then(function(cache) {
             return cache.addAll([
-                '../',
                 '../index.html',
-                '../pages/telaBusca.html',
-                '../pages/telaJogo.html',
-                '../pages/cadastro.html',
-                '../pages/biblioteca.html',
-                '../pages/perfil.html',
-                '../JS/main.js',
-                '../CSS/biblioteca.css',
-                '../CSS/cadastro.css',
-                '../CSS/perfil.css',
-                '../CSS/telaBusca.css',
-                '../CSS/telaJogo.css',
-                '../CSS/Principal.css'
         ]);
     }));
 });
