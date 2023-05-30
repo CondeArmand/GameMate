f66f94ab25ad9803b24dd49e8d1414b7fc098714
Gabriell1507 <gabrielmarcosbguimaraes@gmail.com>
2023-05-30 12:04:22 -0300

    alteração de carrossel

--- JS/PagesJS/principal.js
+++ JS/PagesJS/principal.js
@@ -1,25 +1,65 @@
 import {redirectGamePage} from "../main.js";
+import { getDocumentsGames } from "../FB_Functions/firestore.js";
+// const API_KEY ='5c3cadf5b8e04858a193936f32bad73c';
+// const API_URL = `https://api.rawg.io/api/games?key=${API_KEY}&dates=2023-01-01,2023-12-31`;
 
-const API_KEY ='5c3cadf5b8e04858a193936f32bad73c';
-const API_URL = `https://api.rawg.io/api/games?key=${API_KEY}&dates=2023-01-01,2023-12-31`;
+// const gameCovers = document.getElementById('game-covers');
 
-const gameCovers = document.getElementById('game-covers');
+// fetch(API_URL)
+// 	.then(response => response.json())
+// 	.then(data => {
+// 		data.results.forEach(game => {
+// 			const gameCover = document.createElement('div');
+// 			gameCover.addEventListener('click', () => redirectGamePage(game.id));
+// 			gameCover.classList.add('game-cover');
+// 			gameCover.style.backgroundImage = `url(${game.background_image})`;
+// 			gameCovers.appendChild(gameCover);
+//       const gameTitle = document.createElement('h2');
+//       gameTitle.innerText = game.name;
+//       gameCover.appendChild(gameTitle);
+// 		});
+// 	})
+// 	.catch(error => console.log(error));
+  
 
-fetch(API_URL)
-	.then(response => response.json())
-	.then(data => {
+$(document).ready(function(){
+	var owl = $('.owl-carousel');
+
+	const API_KEY = '5c3cadf5b8e04858a193936f32bad73c';
+	const API_URL = `https://api.rawg.io/api/games?key=${API_KEY}&dates=2023-01-01,2023-12-31`;
+	
+	fetch(API_URL)
+	  .then(response => response.json())
+	  .then(data => {
 		data.results.forEach(game => {
-			const gameCover = document.createElement('div');
-			gameCover.addEventListener('click', () => redirectGamePage(game.id));
-			gameCover.classList.add('game-cover');
-			gameCover.style.backgroundImage = `url(${game.background_image})`;
-			gameCovers.appendChild(gameCover);
-      const gameTitle = document.createElement('h2');
-      gameTitle.innerText = game.name;
-      gameCover.appendChild(gameTitle);
+		  var gameCover = $('<div class="carousel-item"></div>');
+		  gameCover.css('background-image', `url(${game.background_image})`);
+		  var gameTitle = $('<h2></h2>').text(game.name);
+		  gameCover.append(gameTitle);
+		  owl.append(gameCover);
 		});
-	})
-	.catch(error => console.log(error));
   
+		owl.owlCarousel({
+		  loop: true,
+		  margin: 20,
+		  nav: true,
+		  responsive: {
+			0: {
+			  items: 1
+			},
+			600: {
+			  items: 3
+			},
+			1000: {
+			  items: 5
+			}
+		  }
+		});
+	  })
+	  .catch(error => console.log(error));
+  });
 
-  
+  // ...
+
+
+				
