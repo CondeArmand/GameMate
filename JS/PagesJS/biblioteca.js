6e361ab59d43205f50d1e4a00674c71e118cf24e
Conde Armand <neoarmandosousa1010@hotmail.com>
2023-06-07 18:42:11 -0300

    Aviso de resultado não encontrado na biblioteca

--- JS/PagesJS/biblioteca.js
+++ JS/PagesJS/biblioteca.js
@@ -6,24 +6,27 @@ async function renderGames() {
     try {
         const games = await getDocumentsGames();
 
+        if (games.length === 0) {
+            nenhumJogo.style.display = 'flex';
+        } else {
+            games.forEach((game) => {
+                const divCapas = document.querySelector('.capas');
+                const divCapa = document.createElement('div');
+                divCapa.classList.add('capa');
+                divCapa.style.backgroundImage = `url(${game.background_image})`;
+                divCapa.addEventListener('click', () => window.location.href = `../pages/telaJogo.html?id=${game.gameId}`);
 
-        games.forEach((game) => {
-            const divCapas = document.querySelector('.capas');
-            const divCapa = document.createElement('div');
-            divCapa.classList.add('capa');
-            divCapa.style.backgroundImage = `url(${game.background_image})`;
-            divCapa.addEventListener('click', () => window.location.href = `../pages/telaJogo.html?id=${game.gameId}`);
+                const titulo = document.createElement('p');
+                titulo.classList.add('jogo');
+                titulo.innerText = game.name;
 
-            const titulo = document.createElement('p');
-            titulo.classList.add('jogo');
-            titulo.innerText = game.name;
-
-            const divBotoes = document.createElement('p');
-            divBotoes.classList.add('pontos');
-            divBotoes.innerText = '...';
-            divCapa.appendChild(titulo);
-            divCapas.appendChild(divCapa);
-        });
+                const divBotoes = document.createElement('p');
+                divBotoes.classList.add('pontos');
+                divBotoes.innerText = '...';
+                divCapa.appendChild(titulo);
+                divCapas.appendChild(divCapa);
+            });
+        }
     } catch (error) {
         nenhumJogo.style.display = 'flex';
         console.log(error);
